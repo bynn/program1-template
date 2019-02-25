e8db6e07e0ce74b02edc29a88b2189b8204a8d90
nazliguler <42750642+nazliguler@users.noreply.github.com>
2019-02-25 15:21:46 -0500

    Part B

--- List.cpp
+++ List.cpp
@@ -1,1 +1,41 @@
 #include "List.h"
+#include <iostream>
+#include <cstring>
+
+struct Node {
+	Planet * planet;
+	struct Node *next;
+};
+
+List::List(){
+	Node *head = NULL;
+	Node *tail = NULL;
+}
+
+List::~List(){
+	Node * temp = head;
+	while(temp->next != NULL){
+		temp = temp->next;
+		delete temp;
+	}
+}
+
+void insert (int index, Planet * p){
+	int count = 0;
+
+	Node inserting = NULL;
+	inserting->planet = p;
+	Node * previous = NULL;
+	Node * current = NULL;
+
+	current = this->head;
+
+	while(current->next){
+		previous=current;
+		if(count == index){
+			previous->next = current;
+			return;
+		}
+		count = count + 1;
+	}
+}
--- List.h
+++ List.h
@@ -4,8 +4,6 @@
 class List {
 private:
 
-
-
 public:
     List();
     ~List();
@@ -13,7 +11,7 @@ public:
     Planet * read (int index);
     bool remove(int index);
     unsigned size();
-}
+};
 
 
 
